a850b5dd2a4bfa2eacc9dfae3fd42abbae506e5a
Marino Brnardić <marinobrnardic@gmail.com>
2022-11-05 13:12:32 +0100

    Installed axios; Fetched data and populated the table

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,53 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
     <v-main>
-      <HelloWorld/>
+      <v-data-table
+        :headers="headers"
+        :items="devices"
+        :items-per-page="50"
+      ></v-data-table>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import axios from "axios";
 
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
+  name: "App",
   data: () => ({
-    //
+    devices: [],
+    headers: [
+      {
+        text: "Title",
+        align: "start",
+        sortable: true,
+        value: "device.connector.type",
+      },
+      { text: "External Id", align: "center", sortable: true, value: "id" },
+      {
+        text: "Region",
+        align: "end",
+        sortable: true,
+        value: "device.data.region",
+      },
+    ],
   }),
+  methods: {
+    storeData(res) {
+      this.devices = res.data.data;
+    },
+  },
+  mounted() {
+    axios
+      .get("https://xapi.stage.niota.io/xapi/v1/virtual-devices/", {
+        headers: {
+          "x-api-key": "2C2A4B8C-1E11-4C1F-996B-25DD99ED0CC6",
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+      })
+      .then((res) => this.storeData(res));
+  },
 };
 </script>
