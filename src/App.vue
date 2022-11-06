e7578c3cc6b4dc24dff43d5dc34ff4d0e66adce1
Marino Brnardić <marinobrnardic@gmail.com>
2022-11-06 11:49:36 +0100

    Pagination

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <v-app>
     <v-main>
-      <v-data-table
-        :headers="headers"
-        :items="devices"
-        :items-per-page="50"
-      ></v-data-table>
+      <v-container>
+        <v-data-table
+          :headers="headers"
+          :items="devices"
+          :items-per-page="10"
+        ></v-data-table>
+        <v-btn v-if="prevPage" @click="fetchData(prevPage)" class="mr-4"
+          >Previous</v-btn
+        >
+        <v-btn v-if="nextPage" @click="fetchData(nextPage)">Next</v-btn>
+      </v-container>
     </v-main>
   </v-app>
 </template>
@@ -17,6 +23,8 @@ export default {
   name: "App",
   data: () => ({
     devices: [],
+    nextPage: "",
+    prevPage: "",
     headers: [
       {
         text: "Title",
@@ -36,6 +44,21 @@ export default {
   methods: {
     storeData(res) {
       this.devices = res.data.data;
+      this.nextPage = res.data.pagination.next ? res.data.pagination.next : "";
+      this.prevPage = res.data.pagination.previous
+        ? res.data.pagination.previous
+        : "";
+    },
+    fetchData(url) {
+      axios
+        .get(url, {
+          headers: {
+            "x-api-key": "2C2A4B8C-1E11-4C1F-996B-25DD99ED0CC6",
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+          },
+        })
+        .then((res) => this.storeData(res));
     },
   },
   mounted() {
